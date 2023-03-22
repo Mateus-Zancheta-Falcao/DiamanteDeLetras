7eb5fa1cbff95acd42aea306cfc09aea20962a27
Mateus Zancheta Falcao <mateuszancheta2005@gmail.com>
2023-03-22 00:14:24 -0300

    Add project files.

---
+++ DiamanteDeLetras/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamanteDeLetras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a letra desejada: ");
+            char letraEscolhida = Convert.ToChar(Console.ReadLine());
+
+            char letra = 'A';
+
+            int espacosDaMargem = letraEscolhida - letra;
+            int quantidadeLinhas = letraEscolhida - letra;
+            int espacosEntreAsLetras = - 1;
+
+
+            for (int linha = 0; linha < quantidadeLinhas; linha++)
+            {
+                for (int i = 0; i < espacosDaMargem; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(letra);
+
+                for (int i = 0; i < espacosEntreAsLetras; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (letra != 'A')
+                    Console.Write(letra); 
+
+                letra++;
+                espacosDaMargem--;
+                espacosEntreAsLetras+= 2;
+
+                Console.WriteLine();
+            }
+
+            Console.Write(letraEscolhida);
+
+            for (int i = 0; i < espacosEntreAsLetras; i++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write(letraEscolhida);
+
+            letra--;
+            espacosDaMargem++;
+            espacosEntreAsLetras-= 2;
+
+            Console.WriteLine();
+
+            for (int linha = 0; linha < quantidadeLinhas; linha++)
+            {
+                for (int i = 0; i < espacosDaMargem; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(letra);
+
+                for (int i = 0; i < espacosEntreAsLetras; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (letra != 'A')
+                {
+                    Console.Write(letra);
+                }
+
+                letra--;
+                espacosDaMargem++;
+                espacosEntreAsLetras-= 2;
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
